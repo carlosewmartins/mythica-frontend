42e9be303b29109c6da81b95fa066b42f774305e
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-15 14:42:29 -0300

    refactor: altera o nome do projeto para mythica

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('primetail');
+  protected readonly title = signal('mythica');
 }
