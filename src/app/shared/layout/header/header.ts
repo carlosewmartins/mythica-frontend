8aed047002f6687aae911f1961af80fc789f93dd
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-16 17:21:43 -0300

    style: estiliza o cabeçalho

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,60 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
 import { BadgeModule } from 'primeng/badge';
 import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
-import { Ripple } from 'primeng/ripple';
 import { Menubar } from 'primeng/menubar'
 import { ButtonModule } from 'primeng/button'
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.html',
+    styleUrl: './header.scss',
     standalone: true,
     imports: [
       Menubar,
       BadgeModule, 
       AvatarModule,
       InputTextModule,
-      Ripple,
       ButtonModule,
       CommonModule]
 })
-export class Header implements OnInit {
-    items: MenuItem[] | undefined;
-
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'Home',
-                icon: 'pi pi-home',
-            },
-            {
-                label: 'Projects',
-                icon: 'pi pi-search',
-                badge: '3',
-                items: [
-                    {
-                        label: 'Core',
-                        icon: 'pi pi-bolt',
-                        shortcut: '⌘+S',
-                    },
-                    {
-                        label: 'Blocks',
-                        icon: 'pi pi-server',
-                        shortcut: '⌘+B',
-                    },
-                    {
-                        separator: true,
-                    },
-                    {
-                        label: 'UI Kit',
-                        icon: 'pi pi-pencil',
-                        shortcut: '⌘+U',
-                    },
-                ],
-            },
-        ];
-    }
+export class Header {
 }
