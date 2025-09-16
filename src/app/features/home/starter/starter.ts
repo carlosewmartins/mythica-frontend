508b1f6b0ca49e9a030ea88a64eb4738722df711
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-16 17:31:57 -0300

    feat: cria sessão inicial da pagina

---
+++ src/app/features/home/starter/starter.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-starter',
+  imports: [],
+  templateUrl: './starter.html',
+  styleUrl: './starter.scss'
+})
+export class Starter {
+
+}
