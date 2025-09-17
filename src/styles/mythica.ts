cf606222a774ed2a8bf403fd0823b9d24aab5364
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-17 14:33:25 -0300

    style: cor secundaria modificada

--- src/styles/mythica.ts
+++ src/styles/mythica.ts
@@ -4,17 +4,17 @@ import Aura from '@primeuix/themes/aura';
 const MyPreset = definePreset(Aura, {
   semantic: {
     primary: {
-        50: '{amber.50}', 100: '{amber.100}', 200: '{amber.200}',
-        300: '{amber.300}', 400: '{amber.400}', 500: '{amber.500}',
-        600: '{amber.600}', 700: '{amber.700}', 800: '{amber.800}',
-        900: '{amber.900}', 950: '{amber.950}'
+      50: '{amber.50}', 100: '{amber.100}', 200: '{amber.200}',
+      300: '{amber.300}', 400: '{amber.400}', 500: '{amber.500}',
+      600: '{amber.600}', 700: '{amber.700}', 800: '{amber.800}',
+      900: '{amber.900}', 950: '{amber.950}'
     },
     secondary: {
-        50: '{emerald.50}', 100: '{emerald.100}', 200: '{emerald.200}',
-        300: '{emerald.300}', 400: '{emerald.400}', 500: '{emerald.500}',
-        600: '{emerald.600}', 700: '{emerald.700}', 800: '{emerald.800}',
-        900: '{emerald.900}', 950: '{emerald.950}'
-    }  
+      50:  "#F4F6F7", 100: "#E2E7EA", 200: "#C8D1D6",
+      300: "#A5B3BC", 400: "#738391", 500: "#0F1D26",
+      600: "#0D1922", 700: "#0B151C", 800: "#091117",
+      900: "#070D12", 950: "#050A0D"
+    }
   },
    components: {
     button: {
@@ -32,8 +32,16 @@ const MyPreset = definePreset(Aura, {
               hoverBackground: '{primary.800}',
               borderColor: '{primary.700}',
               hoverBorderColor: '{primary.950}',
-              color: '#fff',
-              hoverColor: '{primary.contrast.color}',
+              activeBackground: '{primary.900}',
+              activeBorderColor: '{primary.900}'
+            },
+            secondary: {
+              background: '{secondary.500}',
+              hoverBackground: '{secondary.600}',
+              borderColor: '{slate.600}',
+              hoverBorderColor: '{secondary.600}',
+              activeBackground: '{secondary.700}',
+              activeBorderColor: '{secondary.700}'
             }
           }
         }
@@ -43,35 +51,3 @@ const MyPreset = definePreset(Aura, {
 });
 
 export default MyPreset;
-
-/*
-const MeuTema = definePreset(Aura, {
-    semantic: {
-    secondary: {
-      50: '{emerald.50}', 100: '{emerald.100}', 200: '{emerald.200}',
-      300: '{emerald.300}', 400: '{emerald.400}', 500: '{emerald.500}',
-      600: '{emerald.600}', 700: '{emerald.700}', 800: '{emerald.800}',
-      900: '{emerald.900}', 950: '{emerald.950}'
-    }
-    },
-
-    components: {
-        button: {
-            root: {
-                borderRadius: '12px',
-
-                primary: {
-                    color: '#fff',
-                    background: '{primary.800}'
-                },
-
-                label: {
-                fontWeight: '600'
-                }
-            }
-        }
-    }
-});
-
-export default MeuTema;
-*/
