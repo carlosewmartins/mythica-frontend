273bb56011bed87f60234f047e2507fd39ef62be
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-17 13:50:04 -0300

    feat: importa modulo de botoes do primeng

--- src/app/features/home/starter/starter.ts
+++ src/app/features/home/starter/starter.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-starter',
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './starter.html',
   styleUrl: './starter.scss'
 })
