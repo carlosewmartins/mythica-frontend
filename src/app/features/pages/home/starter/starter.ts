fae13b41d38a33cacaaf710c70165555b57333a0
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-18 18:47:03 -0300

    refactor: altera o diretorio da homepage para app/features/pages para facilitar localizacao de arquivos

---
+++ src/app/features/pages/home/starter/starter.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-starter',
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './starter.html',
+  styleUrl: './starter.scss'
+})
+export class Starter {
+
+}
