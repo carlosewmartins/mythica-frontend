38bbadc5dff770e969dc141697158c9d4e0d4853
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-19 11:28:45 -0300

    style: sobrepõe o cabeçalho sobre a homepage

--- src/app/features/pages/home/starter/starter.ts
+++ src/app/features/pages/home/starter/starter.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { Header } from '../../../../shared/layout/header/header';
 @Component({
   selector: 'app-starter',
   imports: [
-    ButtonModule
+    ButtonModule,
+    Header
   ],
   templateUrl: './starter.html',
   styleUrl: './starter.scss'
