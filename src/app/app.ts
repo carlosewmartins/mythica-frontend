beba779141c9707c898922c7fcef97c980aee599
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-19 12:08:29 -0300

    refactor: remove import do header desnecessario

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './shared/layout/header/header';
 import { Starter } from "./features/pages/home/starter/starter";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Header,
     Starter
 ],
   templateUrl: './app.html',
