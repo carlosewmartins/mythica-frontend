e9627027b0cc8c299b7b616ecb149218ef0c1ef0
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-22 14:46:12 -0300

    refactor: remove import desnecessario de registerform e remove ng-template footer vazio do register-dialog

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Starter } from "./features/pages/home/starter/starter";
-import { RegisterForm } from "./shared/components/register/register-form/register-form";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Starter,
-    RegisterForm
+    Starter
 ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
