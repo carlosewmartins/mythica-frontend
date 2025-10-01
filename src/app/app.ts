852bbe056c221ac52753bcd21b22ebc2ed5b63dd
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-10-01 11:14:46 -0300

    feat: implementa toast(notificação) globalmente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 import { Starter } from "./features/pages/home/starter/starter";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    ToastModule,
     Starter
 ],
   templateUrl: './app.html',
