e38087cba5600fa93f9cbef2d65b1977536f597a
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-10-01 13:04:31 -0300

    feat: cria rota publica para home, retorna para home caso rota não exista, e cria rota protegida para listar personagens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guard/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Rota pública - Home
+  {
+    path: '',
+    loadComponent: () => import('./features/pages/home/starter/starter').then(m => m.Starter),
+    title: 'Mythica'
+  },
+  
+  // Rotas protegidas - Personagens
+  {
+    path: 'personagens',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/pages/personagens/lista-personagens/lista-personagens').then(m => m.ListaPersonagens),
+        title: 'Meus Personagens'
+      }
+    ]
+  },
+  // Pagina não encontrada
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
